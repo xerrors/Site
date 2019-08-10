68a1f29b5c663c66412c58307257adebbdb64790
Xerrors <xerrors@163.com>
2019-08-10 20:56:50 +0800

    deploy

--- deplory.sh
+++ deplory.sh
@@ -17,9 +17,10 @@ cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 echo 'xerrors.fun' > CNAME
 
+git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@git.dev.tencent.com:Xerrors/Site.git master
+git push -f git@git.dev.tencent.com:Xerrors/docs.git master
 
 cd -
