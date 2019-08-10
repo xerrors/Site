f70ce987527936caad3b529e525dac7f59f4e990
Xerrors <xerrors@163.com>
2019-08-10 21:12:56 +0800

    deploy

--- deplory.sh
+++ deplory.sh
@@ -6,21 +6,20 @@ set -e
 # 生成静态文件
 vuepress build docs
 
-git add -A
-git commit -m 'deploy'
-
-git push -f git@git.dev.tencent.com:Xerrors/Site.git master
-
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 echo 'xerrors.fun' > CNAME
 
-git init
 git add -A
 git commit -m 'deploy'
 
 git push -f git@git.dev.tencent.com:Xerrors/docs.git master
 
 cd -
+
+git add -A
+git commit -m 'deploy'
+
+git push -f git@git.dev.tencent.com:Xerrors/Site.git master
