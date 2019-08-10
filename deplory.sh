50b913ad7f9ae2600578ab042270924d17e5032c
Xerrors <xerrors@163.com>
2019-08-10 20:23:01 +0800

    init

--- deplory.sh
+++ deplory.sh
@@ -16,10 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-#  git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@git.dev.tencent.com:Xerrors/Site.git master
 
 cd -
