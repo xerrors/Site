83273053b4f7a38b30bd32c771e8f67e5b4a6b69
Xerrors <xerrors@163.com>
2019-08-11 20:34:29 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,8 +5,8 @@ module.exports = {
     nav: [
       { text: '主页', link: '/' },
       { text: '占位', link: '/placeholder/' },
-      { text: '博客', link: 'https://baidu.com' },
-      { text: 'Github', link: 'https://baidu.com' },
+      { text: '博客', link: 'https://xerrors.github.io' },
+      { text: 'Github', link: 'https://github.com/Xerrors' },
     ]
   }
 }
