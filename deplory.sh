9583abc96ccd9d1bf57b631a441995ee3e87111a
Xerrors <xerrors@163.com>
2019-08-24 15:00:23 +0800

    deploy

--- deplory.sh
+++ deplory.sh
@@ -26,6 +26,12 @@ echo " "
 echo "  >>> 成功将资源文件上传到 git@git.dev.tencent.com:Xerrors/Xerrors.git"
 echo " "
 
+git push -f git@github.com:Xerrors/Xerrors.github.io.git master
+
+echo " "
+echo "  >>> 成功将资源文件上传到 git@github.com:Xerrors/Xerrors.github.io.git "
+echo " "
+
 cd -
 
 git add -A
@@ -37,8 +43,8 @@ echo " "
 echo "  >>> 成功将整体文件上传到 腾讯云 [ git@git.dev.tencent.com:Xerrors/Site.git ]"
 echo " "
 
-git push -f git@github.com:Xerrors/Xerrors.github.io.git master
+git push -f git@github.com:Xerrors/Site.git master
 
 echo " "
-echo "  >>> 成功将资源文件上传到 GitHub [ git@github.com:Xerrors/Xerrors.github.io.git ]"
+echo "  >>> 成功将资源文件上传到 GitHub [ git@github.com:Xerrors/Site.git ]"
 echo " "
