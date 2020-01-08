94cf52d391b9b9a573c41b4e00054a3b4d737762
Xerrors <xerrors@163.com>
2020-01-09 00:27:20 +0800

    changed page style

---
+++ docs/.vuepress/theme/components/Comments.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="vcomment" v-if="data.comments === true">
+    <div id="vcomments"></div>
+  </div>
+</template>
+
+<script>
+import { isActive, hashRE, groupHeaders } from '../util'
+export default {
+  computed: {
+    data () {
+      return this.$page.frontmatter
+    },
+  },
+  mounted: function(){
+    this.createValine()
+  },
+  
+  methods: {
+    createValine() {
+      const Valine = require('valine');
+      window.AV = require('leancloud-storage')
+      const valine =  new Valine({
+        el: '#vcomments',
+        appId: 'lBKeuOkAPgfaieov6tJObJyA-gzGzoHsz',
+        appKey: 'g73DyYQYwPKg3hOwwf9lRY9i',
+        notify: false,
+        verify: false,
+        avatar: 'monsterid',
+        path: window.location.pathname,
+        placeholder: '欢迎留言与我分享您的想法...',
+      });
+      this.valineRefresh = false
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+      if(to.path !==  from.path){
+        setTimeout(() => {
+          //重新刷新valine
+          this.createValine()
+        }, 300)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+#vcomments {
+  max-width 740px
+  padding 10px
+  display block;
+  margin-left auto;
+  margin-right auto;
+}
+</style>
