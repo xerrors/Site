785c765fe63a6513f12ac7ac6b2db8ab48171aff
Xerrors <xerrors@163.com>
2020-02-26 00:20:52 +0800

    update-deplory.sh

--- deplory.sh
+++ deplory.sh
@@ -17,28 +17,10 @@ vuepress build docs
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
-
 cp -r ./* /www/wwwroot/www.xerrors.fun
 
 echo " "
 echo "  >>> 成功将资源文件复制到网站根目录 /www/wwwroot/www.xerrors.fun/"
-echo " "
-
-# 如果是发布到自定义域名
-# echo 'blog.xerrors.fun' > CNAME
-git init
-git add -A
-git commit -m $message
-git push -f git@git.dev.tencent.com:Xerrors/Xerrors.git master
-
-echo " "
-echo "  >>> 成功将资源文件上传到 git@git.dev.tencent.com:Xerrors/Xerrors.git"
-echo " "
-
-git push -f git@github.com:Xerrors/Xerrors.github.io.git master
-
-echo " "
-echo "  >>> 成功将资源文件上传到 git@github.com:Xerrors/Xerrors.github.io.git "
 echo " "
 
 cd -
@@ -46,10 +28,10 @@ cd -
 git add -A
 git commit -m $message
 
-git push -f git@git.dev.tencent.com:Xerrors/Site.git master
+git push -f git@e.coding.net:Xerrors/Site.git master
 
 echo " "
-echo "  >>> 成功将整体文件上传到 腾讯云 [ git@git.dev.tencent.com:Xerrors/Site.git ]"
+echo "  >>> 成功将整体文件上传到 腾讯云 [ git@e.coding.net:Xerrors/Site.git ]"
 echo " "
 
 git push -f git@github.com:Xerrors/Site.git master
@@ -62,6 +44,3 @@ cd ~/Node
 
 forever start zone.js
 
-# cd ~/Chen/docs/.vuepress/dist/
-
-# http-server -p 5000
