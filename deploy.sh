59e7e15ddabee335edf3544b38ccfca7e19e3014
Xerrors <xerrors@163.com>
2020-05-17 21:39:41 +0800

    commited-by-VSCode

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,14 @@
 #!/usr/bin/env sh
 
-if test -z $1
-then
-  message="commited-by-VSCode"
-else
-  message=$1
-fi
-
 cd ~/Site
+
+git pull
 
 # 确保脚本抛出遇到的错误
 set -e
 
-git add -A
-git commit -m $message
-git push -f git@e.coding.net:Xerrors/Site.git master
-
-echo " "
-echo "  >>> 成功将整体文件上传到 腾讯云 [ git@e.coding.net:Xerrors/Site.git ]"
-echo " "
-
-git push -f git@github.com:Xerrors/Site.git master
-
-echo " "
-echo "  >>> 成功将资源文件上传到 GitHub [ git@github.com:Xerrors/Site.git ]"
-echo " "
-
 # 生成静态文件
-vuepress build docs
+yarn docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
